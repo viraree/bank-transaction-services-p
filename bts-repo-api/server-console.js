29ef1401e1ddb8f9f47e7a6babfb740c986a9493
viraree <tao.wang@deckers.com>
2023-07-25 11:51:31 +0800

    Initial commit

---
+++ bts-repo-api/server-console.js
@@ -0,0 +1,17 @@
+// getting-started.js
+//const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  //await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  console.log('svc starting');
+
+ /* await mongoose.connect('mongodb://127.0.0.1:27017');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback () {
+    console.log("h");
+    });*/
+
+}
